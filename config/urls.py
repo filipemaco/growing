69ad62553503685baad4b740646f6841db0e2f7a
filipe <filipe@tonicapp.com>
2020-12-30 21:03:28 +0000

    Update docker

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
 
 from rest_framework import permissions
 
@@ -9,3 +10,10 @@ urlpatterns = [
     path('api/v1/objectives', include('objectives.urls')),
     path('api/v1/users', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
